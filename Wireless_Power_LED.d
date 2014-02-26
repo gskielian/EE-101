b37ea05920db861290c1d5ca3dd6053b7a06eb20
Gregory S. Kielian <gregory.kielian@gmail.com>
2014-02-25 21:17:43 -0800

    Create Wireless_Power_LED.d

---
+++ Wireless_Power_LED.d
@@ -0,0 +1,10 @@
+In this project you'll create a circuit which can wirelessly power an LED.
+
+
+Materials:
+
+* Oscilloscope
+* LED
+* Magnet wire
+* Iron Core
+* Frequency Source (Arduino, or crystal oscillator, or 555)
